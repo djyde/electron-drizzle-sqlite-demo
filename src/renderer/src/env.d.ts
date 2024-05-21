c9b145d0b7468028a38202c9440c04c7dbef440c
imWildCat <wildcat.name@gmail.com>
2024-05-21 10:00:06 -0600

    fix env

--- src/renderer/src/env.d.ts
+++ src/renderer/src/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+interface Window {
+  // expose in the `electron/preload/index.ts`
+  ipcRenderer: import('electron').IpcRenderer
+  electron: import('../main/electron').Electron
+}
